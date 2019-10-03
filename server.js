64e5d4efed3a4228457e26b5aac4c9049d2def40
Vladyslav Sydorenko <Vladyslav_Sydorenko@epam.com>
2019-10-03 17:41:18 +0300

    fix

--- server.js
+++ server.js
@@ -9,8 +9,14 @@ const app = express();
 app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+
+    const fs = require('fs');
+
+res.json({
+    dir : fs.readdirSync(__dirname)
+})
+
+// res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
