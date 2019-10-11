514911537d44b64c316eaccae594125392efac16
Vladyslav Sydorenko <Vladyslav_Sydorenko@epam.com>
2019-10-11 15:37:18 +0300

    fix

--- server.js
+++ server.js
@@ -1,23 +1,10 @@
 
-//Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/lesson1'));
-
-app.get('/*', function(req,res) {
-
-//     const fs = require('fs');
-
-// res.json({
-//     dir : fs.readdirSync(__dirname)
-// })
-
-res.sendFile(path.join(__dirname+'/dist/lesson1/index.html'));
+app.use(express.static(__dirname + '/dist/itea-project'));
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/itea-project/index.html'));
 });
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LolComponent } from './lol/lol.component';
+import { Lol1Component } from './lol1/lol1.component';
+import { Lol2Component } from './lol2/lol2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LolComponent,
+    Lol1Component,
+    Lol2Component
   ],
   imports: [
     BrowserModule,
